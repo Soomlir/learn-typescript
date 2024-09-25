b72ed7c99342eec68210e80658045a7a7be5bf90
Soomlir <Soomlir@yandex.ru>
2024-09-25 19:52:20 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+// const str: string = 'abc';
+// // const num: number = 10;
+// // const bool: boolean = true;
+// const arr: number[] = [1, 2, 3];
+// // const arr2: Array<string> = ['a', 'c'];
+// console.log(arr)
+
+// function getNum(num: number = 10): number {
+//   console.log(num ** 2);
+//   return num;
+// } 
+
+// getNum();
+
+// const obj = {a: 1, b: 2};
+
+// const func = (pt: {x: number; y: number;}) => {
+//     return pt.x * pt.y;
+// };
+
+// console.log(func({x: obj.a, y: obj.b}));
+
+// let test: string = '1';
+// test += 1;
+
+// console.log(test);
+
+// // const obj2 = {
+// //   a: 10,
+// //   b: 20
+// // };
+
+
+// const any: number | null | boolean = 10;
+// console.log(any)
+
+// type str = string;
+// // let tes2t: str = 'abc';
+
+// type nudefinded = null | undefined;
+// // const variable: nudefinded = undefined;
+
+// let status: 'error' | 'warning' | 'success';
+// status = 'success';
+// console.log(status);
+
+// type message = 'success' | 'error';
+// // let str3: message;
+// // str3 = 'success';
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
